aef67c1555aec1cf9e79d0651f9825e8bc772d75
Snaehath <2020ad0556@svce.ac.in>
2025-07-03 22:59:53 +0530

    Updated config for page

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:"/Snaehath-Portfolio",
+  base: "/portfolio/",
   plugins: [react(), tailwindcss()],
   server: {
     open: true,
