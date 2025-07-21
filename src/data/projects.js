cc148fde313c051f39589c36bc8adc38e5c29bd1
Snaehath <2020ad0556@svce.ac.in>
2025-07-21 11:28:24 +0530

    updated projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -7,33 +7,51 @@ import proj6 from "../assets/proj6.png";
 import proj7 from "../assets/proj7.png";
 import proj8 from "../assets/proj8.png";
 import proj9 from "../assets/proj9.png";
+import proj10 from "../assets/proj10.png";
+import proj11 from "../assets/proj11.png";
+import proj12 from "../assets/proj12.png";
 
 export const Projects = [
   {
     image: proj8,
     title: "ReadHub",
     description: "Your digital reading companion with AI-powered features.",
-    tech_stack: "Next.js, Tailwind CSS, Node.js, Express, MongoDB, ShadCN, Gemini API",
+    tech_stack:
+      "Next.js, Tailwind CSS, Node.js, Express, MongoDB, ShadCN, Gemini API",
     link: "https://readhub-frontend.vercel.app/",
   },
   {
-    image: proj9,
-    title: "Recommendation Engine",
-    description: "A content-based recommendation system using cosine similarity.",
-    tech_stack: "Python, Flask, React",
-    link: "https://snaehath.github.io/Recommendation-engine-frontend/",
+    image: proj11,
+    title: "PromptGen",
+    description:
+      "An AI-powered prompt generation tool for text and image creation, with live previews and smart suggestions.",
+    tech_stack:
+      "Next.js, TailwindCSS, ShadCN, Node.js, Express, Gemini API, Pollination",
+    link: "https://github.com/Snaehath/Prompt_Smith-Frontend",
   },
+
   {
     image: proj1,
     title: "Tic-Tac-Toe",
-    description: "A responsive tic-tac-toe game built using JavaScript class methods.",
+    description:
+      "A responsive tic-tac-toe game built using JavaScript class methods.",
     tech_stack: "JavaScript",
     link: "https://snaehath.github.io/Tic-Tac-Toe/",
   },
+ {
+  image: proj10,
+  title: "GitHub Profile DogTag Generator",
+  description:
+    "A fun and customizable Dog Tag generator for your GitHub profile. Easily add flair to your GitHub README or portfolio.",
+  tech_stack: "React, TypeScript, Vite, GitHub API and Tailwind CSS",
+  link: "https://github.com/Snaehath/Github_DogTag_Generator"
+},
+
   {
     image: proj2,
     title: "Ping-Pong",
-    description: "A classic ping-pong game built with JavaScript and HTML5 Canvas.",
+    description:
+      "A classic ping-pong game built with JavaScript and HTML5 Canvas.",
     tech_stack: "JavaScript",
     link: "https://snaehath.github.io/ping-pong/",
   },
@@ -43,6 +61,23 @@ export const Projects = [
     description: "Generates random quotes with a clean, responsive UI.",
     tech_stack: "JavaScript",
     link: "https://snaehath.github.io/Quote-Gen/",
+  },
+  {
+  image: proj12,
+  title: "The Clock",
+  description:
+    "A Digital Clock.",
+  tech_stack: "React, TypeScript, Vite",
+  link: "https://github.com/Snaehath/The-Clock"
+}
+,
+  {
+    image: proj9,
+    title: "Recommendation Engine",
+    description:
+      "A content-based recommendation system using cosine similarity.",
+    tech_stack: "Python, Flask, React",
+    link: "https://snaehath.github.io/Recommendation-engine-frontend/",
   },
   {
     image: proj4,
@@ -61,15 +96,18 @@ export const Projects = [
   {
     image: proj6,
     title: "Todo List",
-    description: "A simple and intuitive Todo list app built using React and TypeScript.",
+    description:
+      "A simple and intuitive Todo list app built using React and TypeScript.",
     tech_stack: "React, TypeScript",
     link: "https://snaehath.github.io/Todo-list/",
   },
   {
     image: proj7,
     title: "Yelp Camp",
-    description: "A full-stack web app for discovering and reviewing campgrounds.",
-    tech_stack: "JavaScript, EJS, Node.js, Express, MongoDB, Bootstrap, Passport.js",
+    description:
+      "A full-stack web app for discovering and reviewing campgrounds.",
+    tech_stack:
+      "JavaScript, EJS, Node.js, Express, MongoDB, Bootstrap, Passport.js",
     link: "https://yelp-camp-wby5.onrender.com",
   },
 ];
